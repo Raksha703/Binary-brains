85417d3a785fd28c8ec956af7cc621d9927aadd8
Raksha703 <85754170+Raksha703@users.noreply.github.com>
2023-01-07 01:31:01 +0530

    Update index.php

--- chatbot/index.php
+++ chatbot/index.php
@@ -38,27 +38,78 @@ if(isset($_POST['talkRes'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Binary Brains ChatBot</title>
+    <title>Document</title>
     <link rel="stylesheet" href="style.css";>
 
     <link rel="stylesheet" type="text/css" href="jquery.convform.css">
     <script type="text/javascript" src="jquery-3.6.3.js"></script>
     <script type="text/javascript" src="custom.js"></script>
     <script type="text/javascript" src="jquery.convform.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Dosis&family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+    <style> @import url('https://fonts.googleapis.com/css2?family=Dosis&family=Roboto:wght@100;300&display=swap'); </style>
+
 </head>
 <body>
-    <img class="chat_icon" src="icon.png">
+    <img class="chat_icon" src="iii.png">
+
+    <img class="bg" src="back.jpg" alt="Chatbot Service" >
+
+    <div class="container">   
+        <h3> Click here to access the assistant </h3>
+    </div>
 
     <div class="chat_box">
         <div class="conv-form-wrapper">
-            
+        
+        <!--
+        <form action="" method="get" class="hidden">
+        -->
         <form action="index.php" method="post" class="hidden">
+            
+            <!-- options
+            <select name="programmer" data-conv-question="So, are you a programmer? (this question will fork the conversation based on your answer)">
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+            </select>
+            <div data-conv-fork="programmer">
+                <div data-conv-case="yes">
+                     <input type="text" data-conv-question="A fellow programmer! Cool." data-no-answer="true">
+                </div>
+                <div data-conv-case="no">
+                    <select name="thought" data-conv-question="Have you ever thought about learning? Programming is fun!">
+                        <option value="yes">Yes</option>
+                        <option value="no">No..</option>
+                    </select>
+                </div>
+            </div>
+            -->
+
+            <!--
+            ---------------------options------------------------------------
+            <select name="category" data-conv-question="How can i help you?">
+                <option value="web">Web</option>
+                <option value="marketing">Digital marketing</option>
+            </select>
+            <div data-conv-fork="category">
+                <div data-conv-case="web">
+                    -------------------------------input data-----------------------------------
+                     <input type="text" name="domain_name" data-conv-question="Please tell me domain name">
+                </div>
+                <div data-conv-case="marketing">
+                    <input type="text" name="company_name" data-conv-question="Please tell me company name">
+                </div>
+            </div>
+            <input type="text" name="name" data-conv-question="Please tell me your name">
+            --------------------------------------------confirmation-----------------------------------
+            <select data-conv-question="Confirm?">
+                <option value="confirm">Confirm</option>
+            </select>
+            -->
 
             <input type="text" name="Hello" data-conv-question="Hello, I am your career assistant.">
 
-            <input type="text" name="name" data-conv-question="May, I know your name?">
-
-            <select name="intro" data-conv-question="Are you confused in choosing your career field? (Yes/No)">
+            <select name="intro" data-conv-question="Are you confused in choosing your career field?">
                 <option value="yes">Yes</option>
                 <option value="No">No</option>
             </select>
@@ -78,7 +129,7 @@ if(isset($_POST['talkRes'])){
                     </select>
                 </div>
                 <div data-conv-case="Arts">
-                    <select name="artChoice" data-conv-question="Which description sits your personality the most? ">
+                    <select name="talkRes" data-conv-question="Which description sits your personality the most? ">
                         <option value="Painting">Engaging in an activity related to creating art, practicing the arts, or demonstrating an art</option>
                         <option value="Designing">Prefer to work alone, solving problems and creating solutions</option>
                         <option value="Music">Music takes your soul to the universe, wings to the mind, flight to the imagination, and life to everything</option>
@@ -87,7 +138,7 @@ if(isset($_POST['talkRes'])){
                     </select>
                 </div>
                 <div data-conv-case="Techie">
-                    <select name="techChoice" data-conv-question="Which description sits your personality the most?">
+                    <select name="talkRes" data-conv-question="Which description sits your personality the most?">
                         <option value="Web">To create creative pages for easy access</option>
                         <option value="App">Creating small icon which functions in wide range</option>
                         <option value="Game">Help in developing out of the box ideas to casual user</option>
@@ -96,7 +147,7 @@ if(isset($_POST['talkRes'])){
                     </select>
                 </div>
                 <div data-conv-case="History">
-                    <select name="hisChoice" data-conv-question="Which description sits your personality the most? ">
+                    <select name="talkRes" data-conv-question="Which description sits your personality the most? ">
                         <option value="Plants">Gets easily attached with plant and want to know their history</option>
                         <option value="Places">Visits place to explore the history</option>
                         <option value="Fabric">Touches and questions about every new fabric</option>
@@ -112,12 +163,6 @@ if(isset($_POST['talkRes'])){
             </select>
 
             <select name="error" data-conv-question="Do you like to find errors or mistakes and resolve? ">
-                <option value="yes">Yes</option>
-                <option value="no">No</option>
-            </select>
-            
-            <!--database se connect krna hai choice ke according message display hona chahiye-->
-            <select name="error" data-conv-question="From the above survey I think you are fit to choose Web Development as your career option.<br>Do you want to know the roadmap for the same? ">
                 <option value="yes">Yes</option>
                 <option value="no">No</option>
             </select>
