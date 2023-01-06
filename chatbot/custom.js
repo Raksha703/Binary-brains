87752a0a5a55ca41808bd59bc70c7ea9fd4e21ae
Raksha703 <85754170+Raksha703@users.noreply.github.com>
2023-01-07 00:33:29 +0530

    Add files via upload

---
+++ chatbot/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.chat_icon').click(function(event){
+    $('.chat_box').toggleClass('active');
+  });
+
+  $('.conv-form-wrapper').convform({selectInputStyle: 'disable'});
+
+});
